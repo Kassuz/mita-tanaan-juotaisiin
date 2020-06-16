86593648c47090b7203fb4ea4dca782b32aa369d
Kasperi Kuusipuro <Kassuz@users.noreply.github.com>
2020-06-17 00:12:23 +0300

    aaaaa

--- DownloadPrices.py
+++ DownloadPrices.py
@@ -22,7 +22,7 @@ def DownloadPrices():
         response = requests.get("https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx")
 
         if response.status_code == 200:
-            print(len(response.content))
+            # print(len(response.content))
             f = open(filename, "wb")
             f.write(response.content)
             print("Download completed")
